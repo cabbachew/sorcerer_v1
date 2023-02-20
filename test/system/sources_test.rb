da12ef36a1e999c813834192f35b596d55a4a1a0
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-19 18:25:48 -0800

    write failing system tests for showing, updating, and destroying sources

--- test/system/sources_test.rb
+++ test/system/sources_test.rb
@@ -1,17 +1,50 @@
 require "application_system_test_case"
 
 class SourcesTest < ApplicationSystemTestCase
+  setup do
+    @source = sources(:first)
+  end
+
   test "Creating a new source" do
     visit sources_path
     assert_selector "h1", text: "Sources"
 
     click_on "New Source"
-    assert_selector "h1", text: "New Source"
+    assert_selector "h1", text: "New source"
 
-    fill_in "Name", with: "Test Source"
-    click_on "Create Source"
+    fill_in "Name", with: "Test source"
+    click_on "Create source"
 
     assert_selector "h1", text: "Sources"
-    assert_text "Test Source"
+    assert_text "Test source"
+  end
+
+  test "Showing a source" do
+    visit sources_path
+    click_link @source.name
+    
+    assert_selector "h1", text: @source.name
+  end
+
+  test "Updating a source" do
+    visit sources_path
+    assert_selector "h1", text: "Sources"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit source"
+
+    fill_in "Name", with: "Updated source"
+    click_on "Update source"
+
+    assert_selector "h1", text: "Sources"
+    assert_text "Updated source"
+  end
+
+  test "Destroying a source" do
+    visit sources_path
+    assert_text @source.name
+
+    click_on "Delete", match: :first
+    assert_no_text @source.name
   end
 end
