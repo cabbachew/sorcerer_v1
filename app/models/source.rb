56451a0883fae3157f922a4598fae943fcf3883e
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-20 16:26:23 -0800

    broadcast created, updated, and deleted sources asynchronously

--- app/models/source.rb
+++ app/models/source.rb
@@ -3,4 +3,8 @@ class Source < ApplicationRecord
 
   # Sort by id in descending order
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_later_to "sources", partial: "sources/source", locals: { source: self }, target: "sources" }
+  after_update_commit -> { broadcast_replace_later_to "sources", partial: "sources/source", locals: { source: self }, target: "source_#{self.id}" }
+  after_destroy_commit -> { broadcast_remove_to "sources", target: "source_#{self.id}" }
 end
